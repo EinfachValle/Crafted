ce0fff14d7a68f199bc5f3a404339e0f31886b18
Valentin Röhle <34622900+EinfachValle@users.noreply.github.com>
2025-06-05 21:50:18 +0200

    feature: Valentin Roehle #4 Implemented Convex live db

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "../../../convex/_generated/api";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallery";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (!documents) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +20,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((doc) => (
+          <span key={doc._id} className="block p-4 border-b">
+            {doc.title}
+          </span>
+        ))}
       </div>
     </div>
   );
